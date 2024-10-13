e93bc226d1ff10fcc8895e0fc51cc2809b9a12fd
vlad <madrussianrap@gmail.com>
2024-10-13 12:27:27 -0700

    First test: login with env variables

---
+++ README.md
@@ -0,0 +1,9 @@
+Run specific test with browser
+npx playwright test login.spec.js --headed
+
+Run all tests with browser
+npx playwright test --headed
+
+Debugger
+npx playwright test login.spec.ts --debug
+npx playwright test --debug
--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,7 +1,25 @@
-const { test, expect } = require('@playwright/test');
+const { test, expect } = require("@playwright/test");
+test.use({ workers: 1 });
+const timeout = 35 * 60 * 1000;
+test.setTimeout(timeout);
+const dotenv = require("dotenv");
+dotenv.config();
 
-test('login', async ({ page }) => {
-    await page.goto('https://bossman.co/');
-  
-    await expect(page).toHaveTitle(/Playwright/);
-  });
+test("login", async ({ page, browser }) => {
+  const url = "https://bossman.co/";
+  await page.goto(url);
+  const loginBtnLandingPage = await page.getByRole("link", { name: "Login" });
+  await loginBtnLandingPage.click();
+
+  const username = await page.getByPlaceholder("Username");
+  await username.fill(process.env.USERNAME);
+  const password = await page.getByPlaceholder("********");
+  await password.fill(process.env.PASSWORD);
+
+  const loginBtnOnLoginPage = await page.getByRole("button", { name: "Login" });
+  await loginBtnOnLoginPage.click();
+
+  await expect(page).toHaveURL(/.*portfolios/);
+  console.log("Success: user landed on portfolios page");
+  browser.close();
+});
