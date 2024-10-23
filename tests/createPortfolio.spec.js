b0ef59cb830f4357d29a3c6526dad62f456150aa
vlad <madrussianrap@gmail.com>
2024-10-23 09:30:09 -0700

    template createPortfolio.spec

--- tests/createPortfolio.spec.js
+++ tests/createPortfolio.spec.js
@@ -10,9 +10,9 @@ import { login } from "../utils";
 test("createPortfolio - empty portfolio", async ({ page }) => {
   await login(page);
 
-  //   TODO: we logged in, now let's create a portfolio
-  //   Verify that the user can create a portfolio by asserting via expect(something).toBeVisible() on the page then screenshot the page
-  // For example the title of a portfolio is visible, without assets  added
+  //    TODO: we logged in, now let's create a portfolio
+  //    Verify that the user can create a portfolio by asserting via expect(something).toBeVisible() on the page, then screenshot the page
+  //    For example the title of a portfolio is visible, without assets  added
   await page.screenshot({
     path: "screenshots/createPortfolio.spec.png",
     fullPage: true,
@@ -20,31 +20,29 @@ test("createPortfolio - empty portfolio", async ({ page }) => {
 });
 
 test("createPortfolio - private portfolio visibility", async ({ page }) => {
-    await login(page);
-  
-    //   TODO: use the same portfolio and make it private ( usually is private by default)
-    //   Verify that the user can create a private portfolio 
-    //   Assert it can NOT be viewed from incognito
-    await page.screenshot({
-      path: "screenshots/createPortfolio.private.spec.png",
-      fullPage: true,
-    });
+  await login(page);
+
+  //   TODO: use the same portfolio and make it private ( usually is private by default)
+  //   Verify that the user can create a private portfolio
+  //   Assert it can NOT be viewed from incognito
+  await page.screenshot({
+    path: "screenshots/createPortfolio.private.spec.png",
+    fullPage: true,
   });
+});
 
 test("createPortfolio - public portfolio visibility", async ({ page }) => {
   await login(page);
 
-  //   TODO: use the same portfolio and make it public
-  //   Verify that the user can create a public portfolio 
-    //   Assert it can be viewed from incognito
+  //    TODO: use the same portfolio and make it public
+  //    Verify that the user can create a public portfolio
+  //    Assert it can be viewed from incognito
 
   await page.screenshot({
     path: "screenshots/createPortfolio.public.spec.png",
     fullPage: true,
   });
 });
-
-
 
 test("createPortfolio - with asset and piechart", async ({ page }) => {
   await login(page);
