5cf7dc2bd7541ad0fb4bf76dfac5db0ecbfacdd8
vlad <madrussianrap@gmail.com>
2024-10-22 22:57:40 -0700

    throwing it all into one method for ease of use

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,18 +1,18 @@
 const { test, expect } = require("@playwright/test");
 test.use({ workers: 1 });
-test.describe.configure({ mode: 'parallel' });
+test.describe.configure({ mode: "parallel" });
 const timeout = 35 * 60 * 1000;
 test.setTimeout(timeout);
 const dotenv = require("dotenv");
 dotenv.config();
 
-test("login", async ({ page, browser }) => {
+const login = async (page) => {
   const url = "https://bossman.co/";
   await page.goto(url);
   const loginBtnLandingPage = await page.getByRole("link", { name: "Login" });
   await loginBtnLandingPage.click();
-  await page.waitForURL('**/login');
-  const loginTitle = await page.getByRole('heading', { name: 'Login' });
+  await page.waitForURL("**/login");
+  const loginTitle = await page.getByRole("heading", { name: "Login" });
   expect(loginTitle).toBeTruthy();
   const username = await page.getByPlaceholder("Username");
   await username.fill(process.env.USERNAME);
@@ -23,4 +23,14 @@ test("login", async ({ page, browser }) => {
   await loginBtnOnLoginPage.click();
   await expect(page).toHaveURL(/.*portfolios/);
   console.log("Success: user landed on portfolios page");
+  // Take a screenshot of the portfolios page
+  await page.screenshot({
+    path: "screenshots/login.spec.png",
+    fullPage: true,
+  });
+  console.log("Screenshot taken: Portfolios page");
+};
+
+test("login", async ({ page }) => {
+  await login(page);
 });
