2e5c5fd292491f502dc3e3a25fbf5112ad5f05d4
vlad <madrussianrap@gmail.com>
2024-10-24 23:46:37 -0700

    timeouts

--- utils.js
+++ utils.js
@@ -9,22 +9,28 @@ export const login = async (page, USER , PASS) => {
     await page.goto(url);
     const loginBtnLandingPage = await page.getByRole("link", { name: "Login" });
     await loginBtnLandingPage.click();
+    await page.waitForTimeout(1000);    
+
     await page.waitForURL("**/login");
     const loginTitle = await page.getByRole("heading", { name: "Login" });
     expect(loginTitle).toBeTruthy();
+
     const username = await page.getByPlaceholder("Username");
     if (USER == null) {
       USER = process.env.USERNAME;
     }
     await username.fill(USER);
+    await page.waitForTimeout(500);    
     if (PASS == null) {
       PASS = process.env.PASSWORD;
     }
     const password = await page.getByPlaceholder("********");
     await password.fill(PASS);
+    await page.waitForTimeout(500);   
   
     const loginBtnOnLoginPage = await page.getByRole("button", { name: "Login" });
     await loginBtnOnLoginPage.click();
+    await page.waitForTimeout(1000);   
     await page.waitForURL('**/portfolios');
     await expect(page).toHaveURL(/.*portfolios/);
     console.log("Success: user logged in and landed on portfolios page");
@@ -37,27 +43,33 @@ export const createPortfolio = async (page, portfolioName,portfolioDescription, 
     const createPortfolioButton = await page.getByRole("button", { name: "Create New Portfolio" });
     await createPortfolioButton.click();
     await page.waitForURL('**/portfolio/new');
-  
+    await page.waitForTimeout(500);   
+
     // Fill in portfolio details and submit
     const portfolioNameInput = await page.getByPlaceholder("Portfolio Name");
     await portfolioNameInput.fill(portfolioName);
+    await page.waitForTimeout(500);   
     const description = await page.getByPlaceholder('Portfolio description')
     await description.fill(portfolioDescription);
+    await page.waitForTimeout(500);   
 
     if(isPublic == true){
     // if checkbox is checked then its a publicly facing portfolio otherwise its private
     const publicCheckbox = await page.getByLabel('Public')
     await publicCheckbox.click();
+    await page.waitForTimeout(500);   
     }
 
     //  create the portfolio
     const createButton = await page.getByRole("button", { name: "Create" });
     await createButton.click();
+    await page.waitForTimeout(500);   
   
     // Verify that the portfolio was created successfully
     await page.waitForURL(/\/portfolio\/\d+$/);
     const portfolioTitle = await page.getByText(portfolioName).nth(0);
     await expect(portfolioTitle).toBeVisible();
+    await page.waitForTimeout(500);   
     console.log("Success: Empty portfolio created and title is visible");
 
  
